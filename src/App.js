3062f240a214701af5e70ade898e38fa0ee15e7f
lutherbarragan <luther0211@hotmail.com>
2021-12-03 15:55:28 -0600

    set up general file structure and created 2 new components: Header, button

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './Filmania.png';
 import './App.css';
+import Header from './components/Header/Header';
 
 function App() {
 	return (
 		<div className='App'>
 			<header className='App-header'>
-				<img src={logo} className='App-logo' alt='logo' />
+				<Header />
 			</header>
 		</div>
 	);
---
+++ src/components/Header/Header.js
@@ -0,0 +1,5 @@
+import logo from '../../assets/filmania-logo.png';
+
+const HomeLogo = () => <img src={logo} className='App-logo' alt='logo' />;
+
+export default HomeLogo;
