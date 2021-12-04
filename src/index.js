e73cd21b06fb77f8e552659fd58affffa633aebc
lutherbarragan <luther0211@hotmail.com>
2021-12-04 15:24:28 -0600

    Removed unneeded files

--- src/index.js
+++ src/index.js
@@ -4,13 +4,9 @@ import ReactDOM from 'react-dom';
 
 // STYLES
 import './normalize.css';
-import './index.css';
 
 // APP
 import App from './App';
-
-// OTHERS
-import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
 	<React.StrictMode>
@@ -18,8 +14,3 @@ ReactDOM.render(
 	</React.StrictMode>,
 	document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
