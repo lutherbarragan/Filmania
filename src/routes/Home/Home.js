35f1a713c0618db1713fa5ccb7948ecaee2b0db6
lutherbarragan <luther0211@hotmail.com>
2021-12-08 21:41:53 -0600

    Replaced font Roboto to Inter / Added Headers 1 and 2 for Home

--- src/routes/Home/Home.js
+++ src/routes/Home/Home.js
@@ -6,7 +6,17 @@ function Main() {
 	return (
 		<StyledHome>
 			<Navbar />
-			<main></main>
+			<main>
+				<h1>
+					Unlimited movies, TV <br /> shows, and more.
+				</h1>
+				<h2>Watch anywhere. Cancel anytime.</h2>
+
+				<form>
+					<h3>Ready to watch? Enter your email to create or restart your membership.</h3>
+					<input type='text' placeholder='Email address' />
+				</form>
+			</main>
 		</StyledHome>
 	);
 }
--- src/routes/Home/Home.styles.js
+++ src/routes/Home/Home.styles.js
@@ -2,16 +2,45 @@ import styled from 'styled-components';
 import bg from '../../assets/background.jpg';
 
 const StyledHome = styled.div`
-	font-family: 'Roboto', sans-serif;
+	font-family: 'Inter', sans-serif;
 	background-color: #222;
 	padding-bottom: 1vh;
 
 	main {
 		height: 99vh;
+		padding-top: 15rem;
 		background-image: linear-gradient(to top, rgba(0, 0, 0, 0.8) 0, rgba(0, 0, 0, 0) 60%, rgba(0, 0, 0, 0.9) 100%),
 			radial-gradient(circle, rgba(0, 0, 0, 0.095) 0%, rgba(0, 0, 0, 0.1) 0%, rgba(0, 0, 0, 0.5) 100%), url(${bg});
 		background-size: cover;
 		background-position: center;
+		text-align: center;
+
+		h1,
+		h2,
+		h3 {
+			color: #fff;
+			font-family: inherit;
+		}
+
+		h1 {
+			font-size: 64px;
+			font-weight: 600;
+		}
+
+		h2 {
+			font-size: 26px;
+			font-weight: 400;
+			margin: 1rem auto;
+		}
+
+		form {
+			padding-top: 0.85rem;
+
+			h3 {
+				font-size: 18px;
+				font-weight: 500;
+			}
+		}
 	}
 `;
 
