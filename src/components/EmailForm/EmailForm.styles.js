e8f4ddbd9ea2f0c094c2ae7c240ec6a02ab4c857
Luther Barragan <luther0211@hotmail.com>
2021-12-13 20:13:57 +0000

    Updated EmailForm Component: Now the entire form element is part of EmailFormComponent

--- src/components/EmailForm/EmailForm.styles.js
+++ src/components/EmailForm/EmailForm.styles.js
@@ -1,27 +1,29 @@
 import styled from 'styled-components';
 
-const StyledEmailForm = styled.div`
-	display: flex;
-	flex-wrap: nowrap;
-	justify-content: center;
-	width: 40vw;
-	height: 70px;
-	margin: 0 auto;
-
-	input {
-		padding: 0 10px;
-		height: 100%;
-		width: 65%;
-	}
-	button {
-		width: 35%;
-		font-size: 30px;
-		background-color: ${props => props.theme.mainRed};
-		color: ${props => props.theme.white};
-
-		&:hover {
-			background-color: ${props => props.theme.secondaryRed};
-			cursor: pointer;
+const StyledEmailForm = styled.form`	
+	div {
+		display: flex;
+		flex-wrap: nowrap;
+		justify-content: center;
+		width: 40vw;
+		height: 70px;
+		margin: 0 auto;
+		
+		input {
+			padding: 0 10px;
+			height: 100%;
+			width: 65%;
+		}
+		button {
+			width: 35%;
+			font-size: 30px;
+			background-color: ${props => props.theme.mainRed};
+			color: ${props => props.theme.white};
+			
+			&:hover {
+				background-color: ${props => props.theme.secondaryRed};
+				cursor: pointer;
+			}
 		}
 	}
 `;
--- src/routes/Home/Home.js
+++ src/routes/Home/Home.js
@@ -13,16 +13,15 @@ function Home() {
 	return (
 		<StyledHome>
 			<Navbar />
+			
 			<main>
 				<h1>
 					Unlimited movies, TV <br /> shows, and more.
 				</h1>
+				
 				<h2>Watch anywhere. Cancel anytime.</h2>
-
-				<form>
-					<h3>Ready to watch? Enter your email to create or restart your membership.</h3>
-					<EmailForm type='text' placeholder='Email address' />
-				</form>
+				
+				<EmailForm />
 			</main>
 
 			<HomeSection>
